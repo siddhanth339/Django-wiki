646791880e6aae5141cbaa0ba38a78c0d183f6d7
siddhanth339 <sidrk339@gmail.com>
2020-08-26 23:39:35 +0530

    Render markDown converted into HTML

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from . import util
 
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def TITLE(request, name):
+    html = util.get_entry(name)
+    if (html != None):
+        return render(request, "encyclopedia/Entry.html",{
+                    "TITLE": name,
+                    "content": markdown2.markdown(html)
+                    })
+    else:
+        raise Http404("Page Not Found!")
