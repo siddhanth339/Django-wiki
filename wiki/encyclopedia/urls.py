18bc159bcaccfa42a9118f77550a07beceebf7cc
siddhanth339 <sidrk339@gmail.com>
2020-08-28 21:11:41 +0530

    Added Search

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.TITLE, name = "TITLE")
+    path("<str:name>", views.TITLE, name = "TITLE"),
+    path('search/', views.search, name = "search")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from . import util
 
 import markdown2
@@ -13,8 +13,27 @@ def TITLE(request, name):
     html = util.get_entry(name)
     if (html != None):
         return render(request, "encyclopedia/Entry.html",{
-                    "TITLE": name,
+                    "ti": name,
                     "content": markdown2.markdown(html)
                     })
     else:
         raise Http404("Page Not Found!")
+
+def search(request):
+    if 'q' in request.GET:
+        html = util.get_entry(request.GET['q'])
+        if (html != None):
+            return render(request, "encyclopedia/Entry.html",{
+            "ti": request.GET['q'],
+            "content": markdown2.markdown(html)
+            })
+        else:
+            available_entries = []
+            query = request.GET['q'].lower()
+            for entry in util.list_entries():
+                if (query in entry.lower()):
+                    available_entries.append(entry)
+            
+            return render(request, "encyclopedia/Results.html", {
+                        "available_entries": available_entries
+                    })
